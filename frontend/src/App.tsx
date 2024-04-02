00d50b40069354fa3efc1e774645901dd221d93c
joelborellis <joelborellis@outlook.com>
2024-04-02 10:32:57 -0500

    added the actual query to the App.tsx page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,7 @@ const App: React.FC = () => {
     <div className="App">
       <header className="App-header">
         <p>
-          Stream Example
+        "What are different strategies I should be considering when approaching a new prospect where we are unfamiliar and they are in the early stages of demand?"
         </p>
       </header>
       <main>
